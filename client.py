84eb6b18e9aa48b75fdfedfe4c42058e11c5c874
Srijit Kundu <srijit2004@gmail.com>
2024-06-23 18:41:08 +0530

    first commit

---
+++ client.py
@@ -0,0 +1,88 @@
+from collections import OrderedDict
+from typing import Dict, Tuple
+from flwr.common import NDArrays, Scalar
+
+import torch
+import flwr as fl
+
+from model import Net, train, test
+
+
+class FlowerClient(fl.client.NumPyClient):
+    """Define a Flower Client."""
+
+    def __init__(self, trainloader, vallodaer, num_classes) -> None:
+        super().__init__()
+
+        # the dataloaders that point to the data associated to this client
+        self.trainloader = trainloader
+        self.valloader = vallodaer
+
+        # a model that is randomly initialised at first
+        self.model = Net(num_classes)
+
+        # figure out if this client has access to GPU support or not
+        if torch.backends.mps.is_available():
+            self.device = "mps"
+
+    def set_parameters(self, parameters):
+        """Receive parameters and apply them to the local model."""
+        params_dict = zip(self.model.state_dict().keys(), parameters)
+
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+
+        self.model.load_state_dict(state_dict, strict=True)
+
+    def get_parameters(self, config: Dict[str, Scalar]):
+        """Extract model parameters and return them as a list of numpy arrays."""
+
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+    def fit(self, parameters, config):
+        """Train model received by the server (parameters) using the data.
+
+        that belongs to this client. Then, send it back to the server.
+        """
+
+        # copy parameters sent by the server into client's local model
+        self.set_parameters(parameters)
+
+        
+        lr = config["lr"]
+        momentum = config["momentum"]
+        epochs = config["local_epochs"]
+
+        
+        optim = torch.optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
+
+        # do local training.
+        
+        train(self.model, self.trainloader, optim, epochs, self.device)
+
+
+        return self.get_parameters({}), len(self.trainloader), {}
+
+    def evaluate(self, parameters: NDArrays, config: Dict[str, Scalar]):
+        self.set_parameters(parameters)
+
+        loss, accuracy = test(self.model, self.valloader, self.device)
+
+        return float(loss), len(self.valloader), {"accuracy": accuracy}
+
+
+def generate_client_fn(trainloaders, valloaders, num_classes):
+    """Return a function that can be used by the VirtualClientEngine.
+
+    to spawn a FlowerClient with client id `cid`.
+    """
+
+    def client_fn(cid: str):
+        
+        return FlowerClient(
+            trainloader=trainloaders[int(cid)],
+            vallodaer=valloaders[int(cid)],
+            num_classes=num_classes,
+        ).to_client()
+
+    # return the function to spawn client
+    return client_fn
---
+++ server.py
@@ -0,0 +1,47 @@
+from collections import OrderedDict
+
+
+from omegaconf import DictConfig
+
+import torch
+
+from model import Net, test
+
+
+def get_on_fit_config(config: DictConfig):
+    """Return function that prepares config to send to clients."""
+
+    def fit_config_fn(server_round: int):
+        
+
+        return {
+            "lr": config.lr,
+            "momentum": config.momentum,
+            "local_epochs": config.local_epochs,
+        }
+
+    return fit_config_fn
+
+
+def get_evaluate_fn(num_classes: int, testloader):
+    """Define function for global evaluation on the server."""
+
+    def evaluate_fn(server_round: int, parameters, config):
+    
+        
+
+        model = Net(num_classes)
+
+        device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+
+        params_dict = zip(model.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.Tensor(v) for k, v in params_dict})
+        model.load_state_dict(state_dict, strict=True)
+
+      
+        loss, accuracy = test(model, testloader, device)
+
+      
+        return loss, {"accuracy": accuracy}
+
+    return evaluate_fn
